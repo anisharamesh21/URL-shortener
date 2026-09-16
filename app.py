5dddbae3fc36db678adb6569039d1f0d3fc1a550
Anisha <anramesh@ucsd.edu>
2026-09-16 14:06:12 -0700

    completed phase 4

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
 from flask import Flask, request, jsonify, redirect
 import string
 import random
+from lrucache import LRUCache
+from rate_limiter import RateLimiter
 
 app = Flask(__name__)
 
 url_map = {}
+cache = LRUCache(capacity=6)
+limiter = RateLimiter(capacity=3, refill_rate=1)
 
 def generate_short_code(length=6):
     characters = string.ascii_letters + string.digits
     short_code = "".join(random.choices(characters, k=length))
     return short_code
-    pass
+    
 
 @app.route("/")
 def home():
@@ -18,20 +22,30 @@ def home():
 
 @app.route("/shorten", methods=["POST"])
 def shorten():
+    address = request.remote_addr
+    print("request from", address)
+    if not limiter.check_request(address):
+        return "Too many requests", 429
     data = request.get_json()
     long_url = data["url"]
     short_code = generate_short_code(length=6)
     url_map[short_code]=long_url
+    cache.put(short_code, long_url)
     return jsonify({"short_code":short_code, "long_url":long_url})
 
 @app.route("/<code>")
 def redirect_to_url(code):
-    if code in url_map:
-        return redirect(url_map[code])
+    cache_check = cache.get(code)
+    if cache_check!=-1:
+        print("cache hit")
+        return redirect(cache_check)
     else:
-        return "404: Not found"
+        if code in url_map:
+            print("cache miss")
+            cache.put(code, url_map[code])
+            return redirect(url_map[code])
+    return "Not found", 404
     
-
 
 if __name__ == "__main__":
     app.run(debug=True)
